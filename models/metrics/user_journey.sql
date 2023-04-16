81abd4510121df56ab0231ea8381ffdc0ab36705
Emma Hunt <emmaghunt95@gmail.com>
2023-04-16 18:41:20 +0100

    Update logic in user journey metric calculation

--- models/metrics/user_journey.sql
+++ models/metrics/user_journey.sql
@@ -1,30 +1,76 @@
-with next_page_host as (
+with web_events as (select * from {{ ref('stg_web_events') }})
+
+, examine_first_session as (
     select     
         device_created_timestamp::date as date_journey_started
         , user_cookie
-        , first_value(page_host_name) over (partition by user_cookie, session_id order by device_created_timestamp asc) as first_page_host
-        , lead(page_host_name) over (partition by user_cookie, session_id order by device_created_timestamp asc) as next_page_view_host_name
-    from stg_web_events
+        , page_host_name
+
+        -- This will be used to identify if the user landed on the main site on the first page in their firstsession
+        , row_number() over (
+            partition by user_cookie
+                , session_id 
+            order by device_created_timestamp asc
+        ) as page_view_ordinal_in_session
+
+        -- This identifies when the user switched platforms onto the discourse site
+        , conditional_change_event(page_host_name) over (
+            partition by user_cookie
+            , session_id 
+            order by device_created_timestamp asc
+        ) as changed_platforms
+    from web_events
     where event_name = 'page_view'
     and session_increment = 1
 )
 
-, count_users_with_journey_to_track as (
+-- This CTE is one row per user_cookie, and has 2 flag columns:
+-- The first indiciates if the user started their journey on the main host platform
+-- The second indicates if the user switched to the discourse platform at some point within their first session
+, valid_user_journeys as (
     select 
         date_journey_started
-        , count(distinct user_cookie) as number_of_users
-        , count_if(first_page_host = 'snowplowanalytics.com' and next_page_view_host_name = 'discourse.snowplowanalytics.com') as users_with_valid_journey
-    from next_page_host
-    group by 1
+        , user_cookie
+
+        -- Use max() as we are aggregating over multiple page views per user
+        , max(
+            case
+                when page_view_ordinal_in_session = 1 and page_host_name = 'snowplowanalytics.com' then 1
+                else 0 
+            end
+        ) as started_journey_on_main_site
+        , max(
+            case
+                when changed_platforms = 1 and page_host_name = 'discourse.snowplowanalytics.com' then 1
+                else 0
+            end
+        ) as switched_to_discource_platform_in_session
+    from examine_first_session
+
+    -- Limit to just the 2 important page views in the user's first session
+    where page_view_ordinal_in_session = 1
+        or changed_platforms = 1
+    group by 1, 2
 )
 
+-- Perform final counts and aggregations
 , final as (
     select 
         date_journey_started
-        , number_of_users
-        , users_with_valid_journey
-        , div0(users_with_valid_journey, number_of_users) * 100 as percent_of_users_with_user_journey
-    from count_users_with_journey_to_track
+        , count(distinct user_cookie) as number_of_users
+        , count_if(
+            started_journey_on_main_site = 1
+                and switched_to_discource_platform_in_session = 1
+        ) as number_of_users_with_valid_user_journey
+        , div0(
+            count_if(
+                started_journey_on_main_site = 1 
+                    and switched_to_discource_platform_in_session = 1
+            )
+            , count(distinct user_cookie)
+        ) * 100 as percent_of_users_with_user_journey
+    from valid_user_journeys
+    group by 1
 )
 
 select * from final
